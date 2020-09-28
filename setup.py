2fde9e30098f7c2c3b367b527fc40d361b5dee55
CraziiAce <63609284+CraziiAce@users.noreply.github.com>
2020-09-27 19:53:09 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Hypyxel", 
-    version="0.0.7",
+    version="0.0.9",
     author="CraziiAce",
     author_email="teddyjraz@gmail.com",
     description="A python wrapper for the Hypixel API",
-    long_description="Usage: \n`from hypyxel import hypyxel`\n`hypyxel.get_endpoints()`",
+    long_description="Usage: \n`from hypyxel import hypixel`\n`hypixel.get_endpoints()`",
     long_description_content_type="text/markdown",
     url="https://github.com/CraziiAce/Hypyxel",
     packages=setuptools.find_packages(),
