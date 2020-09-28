3fc830063302bd1826f852dfff8ac6ddfa60c872
CraziiAce <63609284+CraziiAce@users.noreply.github.com>
2020-09-28 09:38:36 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author="CraziiAce",
     author_email="teddyjraz@gmail.com",
     description="An easy-to-use python wrapper for the Hypixel API",
-    long_description=file: README.md,
+    long_description=read("README.md"),
     long_description_content_type="text/markdown",
     url="https://github.com/CraziiAce/Hypyxel",
     packages=setuptools.find_packages(),
