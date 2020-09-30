28db313ca576f384333c917a18f121cb64ee8662
CraziiAce <63609284+CraziiAce@users.noreply.github.com>
2020-09-30 11:47:49 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="Hypyxel", 
-    version="0.0.13",
+    version="0.0.14",
     author="CraziiAce",
     author_email="teddyjraz@gmail.com",
     description="An easy-to-use python wrapper for the Hypixel API",
