3c12b17450ea3c4e38c0f9f86d8cabcd6e97b6f2
CraziiAce <63609284+CraziiAce@users.noreply.github.com>
2020-09-30 11:59:24 -0600

    AAAAAAAAAAAAAAAAAAAAAAAH

--- hypyxel/utils.py
+++ hypyxel/utils.py
@@ -6,7 +6,7 @@ key = ""
 
 def _get_uuid(username):
     try:
-        return requests.request("GET", f"https://playerdb.co/api/player/minecraft/{username}")['data']['player']['raw_id']
+        return requests.request("GET", f"https://playerdb.co/api/player/minecraft/{username}").json()['data']['player']['raw_id']
     except:
         raise UUIDNotFoundError(f"A UUID could not be found for {username}")
 
