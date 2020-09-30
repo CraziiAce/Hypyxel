5107c278fb57fff3d4591c72d166608c2dd582a5
CraziiAce <63609284+CraziiAce@users.noreply.github.com>
2020-09-30 14:56:59 -0600

    Update utils.py

--- hypyxel/utils.py
+++ hypyxel/utils.py
@@ -10,20 +10,15 @@ class Utils:
     """A base class for utility functions"""
 
     
-    def __init__(self):
-        self.key = []
+    def __init__(self, api_key):
+        self.key = api_key
 
     def _get_uuid(self, username):
         try:
             return requests.request("GET", f"https://playerdb.co/api/player/minecraft/{username}").json()['data']['player']['raw_id']
         except:
             raise UUIDNotFoundError(f"A UUID could not be found for {username}")
-
-    def set_api_key(self, user_key):
-        """Set the API key"""
-        self.key.clear()
-        self.key.append(user_key)
-
+            
     def _key_check(self):
         """An internal function to check if the key exists"""
         if not key:
