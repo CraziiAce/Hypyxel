13598706554a9d0796c3c1faa6b6b24ccde20f35
CraziiAce <63609284+CraziiAce@users.noreply.github.com>
2020-09-30 14:34:31 -0600

    Update utils.py

--- hypyxel/utils.py
+++ hypyxel/utils.py
@@ -1,28 +1,34 @@
+"""
+© 2020 CraziiAce
+This code may be used freely as long as long it bears this copyright statement at the start.
+"""
+
 import requests
 
 from hypyxel.errors import UUIDNotFoundError, ApiKeyError
 
-def init():
-    global key
-    key = ""
+Class Utils():
+    """A base class for utility functions"""
 
-def _get_uuid(username):
-    try:
-        return requests.request("GET", f"https://playerdb.co/api/player/minecraft/{username}").json()['data']['player']['raw_id']
-    except:
-        raise UUIDNotFoundError(f"A UUID could not be found for {username}")
+    
+    def __init__(self):
+        self.key = []
 
-def set_api_key(user_key):
-    """Set the API key"""
-    init()
-    global key
-    key = user_key
+    def _get_uuid(username):
+        try:
+            return requests.request("GET", f"https://playerdb.co/api/player/minecraft/{username}").json()['data']['player']['raw_id']
+        except:
+            raise UUIDNotFoundError(f"A UUID could not be found for {username}")
 
-def _key_check():
-    """An internal function to check if the key exists"""
-    if not key:
-        raise ApiKeyError("You need to set the key with set_api_key()")
+    def set_api_key(user_key):
+        """Set the API key"""
+        self.key.clear()
+        self.key.append(user_key)
 
-def _get_key():
-    global key
-    return key
+    def _key_check():
+        """An internal function to check if the key exists"""
+        if not key:
+            raise ApiKeyError("You need to set the key with set_api_key()")
+
+    def _get_key():
+        return self.key[0]
